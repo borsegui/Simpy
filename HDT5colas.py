4afd08833df633e7dcb420cbe0adac739ca5b3a3
valyas01 <johana.ve96@gmail.com>
2015-08-27 17:02:48 -0600

    Create HDT5colas.py
    definicion de variables para control de procesos a ejecutar

---
+++ HDT5colas.py
@@ -0,0 +1,13 @@
+# Universidad del valle de Guatemala
+#Algoritmos y estructura de datos
+# Alejandro de Leon 14345 , Yasmin Valdez 14079
+# Hoja de trabajo # 5 
+# Se utilizo como referencia ejemplos vistos en clase
+
+import simpy
+import random
+
+#Variables para ejecucción de procesos de un sistema operativo
+RANDOM_SEED=42
+CREATE_PROCESS=25
+INTERVAL_PRO = 10.0
