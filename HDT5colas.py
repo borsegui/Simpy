d616d5b4cc764e8c16c7e51abe5c4193a175ff6c
valyas01 <johana.ve96@gmail.com>
2015-08-27 17:50:36 -0600

    update, se agregaron def de generador  de proceso

--- HDT5colas.py
+++ HDT5colas.py
@@ -4,10 +4,26 @@
 # Hoja de trabajo # 5 
 # Se utilizo como referencia ejemplos vistos en clase
 
-import simpy
+# se importan las clases 
 import random
+import simpy
 
-#Variables para ejecucción de procesos de un sistema operativo
-RANDOM_SEED=42
-CREATE_PROCESS=25
-INTERVAL_PRO = 10.0
+# P se refiere al proceso
+
+#declaracion de variables para control de ejecuccion de procesos
+RANDOM_SEED = 42
+CREATE_PROCESS = 150
+INTERVALO_PRO = 10.0 
+
+# genera procesos con lo requisitos planteados en la hoja de trabajo
+def generador(enviroment, NumPro, intervalo, Memoria ,CPU, WTime):
+    for i in range(NumPro):
+        #Aleatorios de instrucciones y de memoria 
+        CRandom = random.randint(1,10) 
+        CMemory = random.randint(1,10) 
+        Generator = proceso(enviroment, 'Proceso:%02d' % i, CMemory, Memoria ,CPU, WTime, CRandom)
+        enviroment.process(Generator)
+        timeSimulator = random.expovariate(1.0 / intervalo) 
+        yield enviroment.timeout(timeSimulator)
+
+
