79046deb060796ef79835d5b6000970c2a4ddb9d
valyas01 <johana.ve96@gmail.com>
2015-08-27 18:03:21 -0600

    update con def principal de proceso

--- HDT5colas.py
+++ HDT5colas.py
@@ -26,4 +26,51 @@ def generador(enviroment, NumPro, intervalo, Memoria ,CPU, WTime):
         timeSimulator = random.expovariate(1.0 / intervalo) 
         yield enviroment.timeout(timeSimulator)
 
+def proceso(enviroment, InstLL, CMemory, Memoria, CPU, WTime, CRandom):
+    global SimuladorT, SimuladorDEV #simulador para controlar el tiempo de ejecucion
+    #enviroment como tiempo actual
+    NProcess = enviroment.now
+    #contro de la RAM
+    DispoRAM = Memoria.capacity - CMemory
+    print('P: %7.4fs %s: Proceso/Llegada inst >>' % (NProcess, InstLL))
+    print('P: %7.4fs %s: RAM req %s / RAM dis  %s' % (NProcess, InstLL, CRandom, DispoRAM)) 
+    # Obtiene la RAM y usa el control
+    with Memoria.get(CMemory) as MemoUS:     
+        yield MemoUS
+        EsperaT = enviroment.now - NProcess  # tiempo de espera, se incia proceso y se pide RAM      
+        
+        print('P: %7.4fs %s: Event: Ready >> Request RAM %6.3f' % (NProcess, InstLL, Memoria.level))
+        # control de instrucciones de proceso
+        while CRandom > 0:
+            with CPU.request() as reqCPU: # Request para el CPU
+                yield reqCPU
+                print('P: %7.4fs %s: Running Event %6.3f' % (NProcess, InstLL, CRandom))
+
+                yield enviroment.timeout(1)
+                # Interrupt de los procesos en caso necesro 
+                if CRandom > 3:
+                    CRandom = CRandom - 3
+                else:
+                    CRandom = 0
+            # control del proceso segun valores dados
+            if CRandom > 0:
+                PROWait = random.randint(1,2) # numero aleatorio para ver si espera o la inst se termina
+                if PROWait == 1: 
+                    with WTime.request() as TMRequest: 
+                        yield TMRequest 
+                        print ('P: %7.4fs %s: Waiting... >>' % (NProcess, InstLL))
+
+                        yield enviroment.timeout(1)
+              
+                print('P: %7.4fs %s: Ready >> Continue >>' % (NProcess, InstLL))
+        ProcTime = enviroment.now - NProcess
+        print ('p: %7.4fs %s:INST Terminada, ExecutionT >> %s' % (enviroment.now, InstLL, ProcTime))
+		
+        with Memoria.put(CMemory) as RefreshRAM:
+            yield RefreshRAM
+            print ('P: %7.4fs %s: Liberando RAM %s' % (enviroment.now, InstLL, CMemory))
+			
+        SimuladorT = SimuladorT + (enviroment.now - NProcess)
+        SimuladorDEV = (SimuladorT*SimuladorT) + (SimuladorDEV)
+        print('P: %7.4fs Memoria utilizada / Revision RAM%6.3f >> tiempo Actual %s' % (enviroment.now, Memoria.level, SimuladorT))  
 
